f00aacc912305c538b3ba03ac14f817f3c953906
ApsaraWitharana <asachini095@gmail.com>
2025-03-21 19:26:15 +0530

    add-user manage function api coll impl

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const userSchema = mongoose.Schema({
     name:{type:String,required:true},
     email:{type:String,required:true,unique:true},
     imageUrl:{type:String,required:true},
-    cartItems:{type:Array,required:true},
+    cartItems:{type:Object,default:{}},
 },{minimize:false})
 
 const User =mongoose.models.user || mongoose.model("user",userSchema);
