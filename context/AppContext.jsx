3c786bd295c0367b8868ac43fd9303b9115124f7
ApsaraWitharana <asachini095@gmail.com>
2025-03-27 13:01:20 +0530

    add- user role base Authorization add token

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -2,7 +2,9 @@
 import { productsDummyData, userDummyData } from "@/assets/assets";
 import { useRouter } from "next/navigation";
 import { createContext, useContext, useEffect, useState } from "react";
-import {useUser} from "@clerk/nextjs";
+import {useAuth, useUser} from "@clerk/nextjs";
+import axios, {get} from "axios";
+import toast from "react-hot-toast";
 
 export const AppContext = createContext();
 
@@ -15,17 +17,39 @@ export const AppContextProvider = (props) => {
     const currency = process.env.NEXT_PUBLIC_CURRENCY
     const router = useRouter()
     const {user} = useUser()
+    const {getToken} = useAuth()
+
     const [products, setProducts] = useState([])
     const [userData, setUserData] = useState(false)
-    const [isSeller, setIsSeller] = useState(true)
+    const [isSeller, setIsSeller] = useState(false)
     const [cartItems, setCartItems] = useState({})
 
     const fetchProductData = async () => {
         setProducts(productsDummyData)
     }
 
+    // set user role base authentication
     const fetchUserData = async () => {
-        setUserData(userDummyData)
+       try {
+           if(user.publicMetadata.role === "seller") {
+               setIsSeller(true)
+           }
+
+           const token = await getToken()
+           const {data} = axios.get('/api/user/data',
+               {headers:{Authorization:`Bearer ${token}`}
+               });
+
+           if (data.success) {
+               setUserData(data.user)
+               setCartItems(data.user.cartItems)
+           }else {
+               toast.error(data.message)
+           }
+       }catch(err){
+           toast.error(err.message)
+
+       }
     }
 
     const addToCart = async (itemId) => {
@@ -79,11 +103,14 @@ export const AppContextProvider = (props) => {
     }, [])
 
     useEffect(() => {
-        fetchUserData()
-    }, [])
+        if (user){
+            fetchUserData()
+        }
+
+    }, [user])
 
     const value = {
-        user,
+        user,getToken,
         currency, router,
         isSeller, setIsSeller,
         userData, fetchUserData,
